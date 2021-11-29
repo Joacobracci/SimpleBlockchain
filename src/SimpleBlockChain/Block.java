2c970d2249e2109f707ae8c756fa4dc67db34ac7
Joaquin <56325043+Joacobracci@users.noreply.github.com>
2021-11-29 02:21:10 -0300

    Update Block.java
    Agregamos el metodo minar bloque

--- src/SimpleBlockChain/Block.java
+++ src/SimpleBlockChain/Block.java
@@ -9,7 +9,25 @@ public class Block {
 		private String datos; // datos del mensaje
 		private long timeStamp; // marca de tiempo en segundos desde 1 1 1970
 		
+		private int nonce;
 		
+		// Metodo Constructor
+		
+		public Block(String datos, String hashAnterior) {
+						
+				
+			this.hashAnterior = hashAnterior;
+			this.datos = datos;
+			this.timeStamp = new Date().getTime();
+			this.hash = calcularHash(); 
+						
+						
+			// metodo constructo del bloque, con el hash anterior, los datos y el tiempo.
+			// asumo que el hash se va a generar a partir del hashanterior para que si este cambia 
+			// o alguien altere el hash, se arruine la cadena y se pueda detectar.
+						
+			}
+								
 		public String calcularHash() {
 			
 			String hashCalculado = StringUtil.applicarSha256(
@@ -18,25 +36,25 @@ public class Block {
 						datos
 					
 					);
+			
 			return hashCalculado;
 		}
+		
 		// Le aplicamos el fucking SHA256
 		
-		// Metodo Constructor
+		// el metodo toma un int llamado dificultad, este es el numero de 0 que tiene que resolver.
 		
-			public Block(String datos, String hashAnterior) {
-				
-		
-				this.hashAnterior = hashAnterior;
-				this.datos = datos;
-				this.timeStamp = new Date().getTime();
-				this.hash = calcularHash(); 
-				
-				
-				// metodo constructo del bloque, con el hash anterior, los datos y el tiempo.
-				// asumo que el hash se va a generar a partir del hashanterior para que si este cambia 
-				// o alguien altere el hash, se arruine la cadena y se pueda detectar.
-				
+		public void minarBloque(int dificultad) {
+			String target = new String(new char[dificultad]).replace('\0','0');
+			// Creamos un string con dificultad * "0" 
+			
+			while(!hash.substring(0, dificultad).equals(target) ) {
+				nonce ++;
+				hash = calcularHash();
 			}
+			// Mientras que el bloque no este minado que siga minando xD algo asi no se explicarlo.
+			
+			System.out.println("Bloque Minado!!!!!!" + hash);
+		}
 			
 }

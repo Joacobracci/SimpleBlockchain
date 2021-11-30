600fb32c6089e7d5758effb258f3c6b284a97d74
Joaquin <56325043+Joacobracci@users.noreply.github.com>
2021-11-30 03:45:42 -0300

    Update Wallet.java

--- src/SimpleBlockChain/Wallet.java
+++ src/SimpleBlockChain/Wallet.java
@@ -1,7 +1,10 @@
 package SimpleBlockChain;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import SimpleBlockChain.JoacoChain;
 
 
 public class Wallet {
@@ -10,6 +13,8 @@ public class Wallet {
 	
 	public PrivateKey privateKey; // se usa para firmar las transacciones
 	public PublicKey publicKey; // es nuestra address , nuestra direccion.
+	
+	public HashMap<String,TransactionOutput> UTXOs = new HashMap<String,TransactionOutput>(); //solo UTXOs de esta wallet
 	
 	public Wallet() {
 		
@@ -45,5 +50,48 @@ public class Wallet {
 		}
 	}
 	
+	// Devuelve el balance y guarda el UTXO owned por esta wallet en this.UTXO
+	
+	public float getBalance() {
+			float total=0;
+		for(Map.Entry<String, TransactionOutput> item: JoacoChain.UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			if(UTXO.isMine(publicKey)) { // si la moneda es mia 
+				UTXOs.put(UTXO.id,UTXO); // agregala a la lista de las transacciones unspend
+				total += UTXO.value;
+			}
+		}
+		return total;
+	}
+	
+	// Genera y devuelve una nueva transaccion para esta wallet
+	public Transaction sendFunds(PublicKey _recipient,float value) {
+		if(getBalance() < value) { // reunir el saldo y chequear fondos
+			System.out.println("#No hay suficientes fondos para enviar esta transaccion, transaccion declinada.");
+			return null;
+			
+		}
+		
+		//Crear un ArrayList de Entradas
+		ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+		
+		float total = 0;
+		for(Map.Entry<String, TransactionOutput> item : UTXOs.entrySet()) {
+			TransactionOutput UTXO = item.getValue();
+			total += UTXO.value;
+			inputs.add(new TransactionInput(UTXO.id));
+			if(total > value) break;
+			
+		}
+		
+		Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+		newTransaction.generateSignature(privateKey);
+		
+		for(TransactionInput input: inputs) {
+			UTXOs.remove(input.transactionOutputId);
+		}
+		
+		return newTransaction;
+	}
 
 }
